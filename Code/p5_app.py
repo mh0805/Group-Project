3d8ff2e3a23dfb92708cb3e2070eddb3fa924141
Hans Baumberger <hansbaumberger@Hanss-MacBook-Pro.local>
2022-01-06 19:59:24 -0500

    almost done

--- Code/p5_app.py
+++ Code/p5_app.py
@@ -2,12 +2,17 @@ import pandas as pd
 import pickle
 import streamlit as st
 
-# path difference b/c solution code
-with open(#DATA path
-    , 'rb') as pickle_in:
-        pipe/#MODEL NAME OR PIPELINE
-        = pickle.load(pickle_in)
 
+with open('../Models/totalcomp_gs_gbr2.pkl', 'rb') as f:
+    gs_gbr2 = pickle.load(f)
+        
+year = 2020
+month = 9
+inflation_rate = 0.014
+inflation_rate_3mos = 0.006
+employment_rate = 0.93582054649934
+employment_rate_3mos = 0.8939803405002691  
+        
 st.sidebar.header('Total compensation Predictor')
 select = st.sidebar.selectbox('Select Form', ['Form 1'], key='1')
 if not st.sidebar.checkbox("Hide", True, key='1'):
@@ -18,10 +23,15 @@ if not st.sidebar.checkbox("Hide", True, key='1'):
     years_at_company =  st.number_input("Years at company:")
     state = st.selectbox(label = 'US State:', options = df[state_short].unique())
 submit = st.button('Predict')
-if submit:
-        prediction = #MODEL Name
-        .predict([[company_name, title, years_of_experience, years_at_company, state]])
-        st.write('You can expect a total compensation of' prediction '$.')
 
-df['inflation_rate'].iloc[-1] #Code for hardcore input data for modeling prediction        
-#https://towardsdatascience.com/diabetes-prediction-application-using-streamlit-fed6120124a5
+if submit:
+        
+    new_data_df = pd.DataFrame([company_name, title, years_of_experience, years_at_company, year, month, state, inflation_rate,employment_rate,employment_rate_3mos])
+
+
+    new_data_df_ct = ct.transform(new_data_df)
+    gs_gbr2.predict(new_data_df_ct)
+        
+    prediction = gs_gbr2.predict(new_data_df_ct)
+    st.write('You can expect a total compensation of' prediction '$.')
+
